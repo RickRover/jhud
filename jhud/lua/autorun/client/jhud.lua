f2a4005af7d5bb91cefaa4c08a2c6a28114369e8
RickRover <67151570+RickRover@users.noreply.github.com>
2024-02-25 00:39:52 +0300

    Add files via upload

--- jhud/lua/autorun/client/jhud.lua
+++ jhud/lua/autorun/client/jhud.lua
@@ -1,11 +1,14 @@
 local hide = {
 	["CHudHealth"] = true,
-	["CHudBattery"] = true
+	["CHudBattery"] = true,
+    ["CHudAmmo"] = true,
+    ["CHudSecondaryAmmo"] = true
 }
 local sn_health = 100
 local sn_armor = 100
 local x = 50
 local y = ScrH() - 70
+local cols = {Color(0, 0, 0, 80), Color(255,255,255,255), Color(255, 50, 50, 200), Color(50, 50, 255, 200), Color(78, 92, 115, 255)}
 
 local textLicense = {
     have = "Есть лицензия",
@@ -71,34 +74,35 @@ local function HUD()
         surface.SetMaterial(license_icon)
         surface.DrawTexturedRect(x + 36, y - 78, 32, 32)
 
-        draw.SimpleText(ply:getDarkRPVar("money") .. '$ + ' .. ply:getDarkRPVar("salary") .. '$', "HUDInfo", x + 72, y - 2, Color(255,255,255,255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+        draw.SimpleText(ply:getDarkRPVar("money") .. '$ + ' .. ply:getDarkRPVar("salary") .. '$', "HUDInfo", x + 72, y - 2, cols[2], TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
         draw.SimpleText(ply:getDarkRPVar("job"), "HUDInfo", x + 72, y - 38, Color(team_colors.r, team_colors.g, team_colors.b, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
-        draw.SimpleText((ply:getDarkRPVar("HasGunlicense")) and textLicense.have or textLicense.notHave, "HUDInfo", x + 72, y - 74, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
-        draw.SimpleText(ply:Name(), "HUDInfo", x + 72, y + 32, Color(255,255,255,255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)    
+        draw.SimpleText((ply:getDarkRPVar("HasGunlicense")) and textLicense.have or textLicense.notHave, "HUDInfo", x + 72, y - 74, cols[2], TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
     end
 
     local y_1 = y - 2*sn_health
     local y_2 = y - 2*sn_armor
-    draw.RoundedBox(3, x - 29, y - 2*100 + 26, 20, 200, Color(0, 0, 0, 80))
-    draw.RoundedBox(3, x + 6, y - 2*100 + 26, 20, 200, Color(0, 0, 0, 80))
-    draw.RoundedBox(3, x - 29, y_1 + 26, 20, 2*sn_health, Color(255, 50, 50, 200))
-    draw.RoundedBox(3, x + 6, y_2 + 26, 20, 2*sn_armor, Color(50, 50, 255, 200))
+    draw.RoundedBox(3, x - 29, y - 2*100 + 26, 20, 200, cols[1])
+    draw.RoundedBox(3, x + 6, y - 2*100 + 26, 20, 200, cols[1])
+    draw.RoundedBox(3, x - 29, y_1 + 26, 20, 2*sn_health, cols[3])
+    draw.RoundedBox(3, x + 6, y_2 + 26, 20, 2*sn_armor, cols[4])
+
+    draw.SimpleText(ply:Name(), "HUDInfo", x + 72, y + 32, cols[2], TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
 
     if !IsValid(wep) then return end
     -- GUN ICON
-    draw.RoundedBox(20, ScrW() - 252, y - 26, 36, 36, Color(78, 92, 115, 255))
+    draw.RoundedBox(20, ScrW() - 252, y - 26, 36, 36, cols[5])
     surface.SetDrawColor(255, 255, 255)
     surface.SetMaterial(gun_icon)
     surface.DrawTexturedRect(ScrW() - 250, y - 24, 32, 32)
     -- AMMO ICON
     if wep:GetMaxClip1() > 0 then
-         draw.RoundedBox(20, ScrW() - 252, y + 19, 36, 36, Color(78, 92, 115, 255))
+         draw.RoundedBox(20, ScrW() - 252, y + 19, 36, 36, cols[5])
          surface.SetDrawColor(255, 255, 255)
          surface.SetMaterial(ammo_icon)
          surface.DrawTexturedRect(ScrW() - 250, y + 19, 32, 32)
-        draw.SimpleText(wep:Clip1() .. ' / ' .. ply:GetAmmoCount(wep:GetPrimaryAmmoType()), "HUDInfo", ScrW() - 212, y + 25, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+        draw.SimpleText(wep:Clip1() .. ' / ' .. ply:GetAmmoCount(wep:GetPrimaryAmmoType()), "HUDInfo", ScrW() - 212, y + 25, cols[2], TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
     end
-    draw.SimpleText(wep:GetPrintName(), "HUDInfo", ScrW() - 212, y - 20, Color(255, 255, 255, 255), TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
+    draw.SimpleText(wep:GetPrintName(), "HUDInfo", ScrW() - 212, y - 20, cols[2], TEXT_ALIGN_LEFT, TEXT_ALIGN_TOP)
 end
 
 hook.Add("HUDShouldDraw", "HideHUD", function(name)
